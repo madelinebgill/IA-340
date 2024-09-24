00f8c1ec217ce56aa9d8022cf81988ee868e5cad
madelinebgill <gillmb@dukes.jmu.edu>
2024-09-24 18:24:18 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -3,3 +3,19 @@
 
 --Q4.2
 /* No, because there is no fips where the value is 80 */ 
+
+--Q4.3
+/* No, they are not perfect */
+
+--Q4.5
+/* Yes, ChatGPT recognized the chart */
+
+--Q4.6
+/* Yes, ChatGPT gave the right result after it made changes to the code */ 
+
+--Q4.7
+/* The prompt is calculate the year-over-year population growth rate for the VA state in the recent 5 years.
+We revised the prompt to include the table names and the filed names from the diagram */
+
+--Q4.8
+/* No, ChatGPT does not always produce accurate results */ 
